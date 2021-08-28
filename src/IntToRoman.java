eeac1f357d1b4e0cf45820b4c2aef90a299d26b3
a-baade <andreasbaade@hotmail.com>
2021-08-28 20:12:35 +0200

    allTests(passed)

--- src/IntToRoman.java
+++ src/IntToRoman.java
@@ -1,7 +1,20 @@
 public class IntToRoman {
 
-    public String toRoman(int i) {
-        return "I";
+    public String toRoman(int num) {
+        int[] values = {100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] romanNum = {"C", "XC", "L", "XL", "X", "IX", "V","IV", "I"};
+
+        String roman = "";
+
+        for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {
+                num -= values[i];
+                roman += romanNum[i];
+            }
+        }
+        return roman;
     }
 }
+
+
 
--- test/toRomanTest.java
+++ test/toRomanTest.java
@@ -15,6 +15,34 @@ public class toRomanTest {
     public void shouldConvert1ToI() {assertEquals("I", roman.toRoman(1));
     }
 
+    @Test
+    public void shouldConvert2ToII() {assertEquals("II", roman.toRoman(2));
+    }
+
+    @Test
+    public void shouldConvert3ToIII() {assertEquals("III", roman.toRoman(3));
+    }
+    @Test
+    public void shouldConvert4ToIV() {assertEquals("IV", roman.toRoman(4));
+    }
+    @Test
+    public void shouldConvert5ToV() {assertEquals("V", roman.toRoman(5));
+    }
+    @Test
+    public void shouldConvert8ToVIII() {assertEquals("VIII", roman.toRoman(8));
+    }
+
+    @Test
+    public void shouldConvert9ToIX() {assertEquals("IX", roman.toRoman(9));
+    }
+
+    @Test
+    public void shouldConvert10ToX() {assertEquals("X", roman.toRoman(10));
+    }
+    @Test
+    public void shouldConvert37ToXXXVII() {assertEquals("XXXVII", roman.toRoman(37));
+    }
+
 
 }
 
